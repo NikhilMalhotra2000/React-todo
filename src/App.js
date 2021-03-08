ec2f99e92ba8407e63e23816d30e159c85c37ead
Nikhil Malhotra <nikhil1191.cse18@chitkara.edu.in>
2021-03-09 00:14:16 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{useState} from "react";
+import './App.css';
+//importing components
+import Form from './components/Form';
+import TodoList from "./components/TodoList";
+
+function App() {
+  const [inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState([]);
+  return (
+    <div className="App">
+     <header><h1>Nick's Todo List</h1></header>
+     <Form todos={todos} setInputText={setInputText} setTodos={setTodos} inputText={inputText} />
+     <TodoList todos={todos} />
+    </div>
+  );
+}
+
+export default App;
